0274c37352a3c289ebbc92fde454e9cddf3a4e46
mfortea <43608081+mfortea@users.noreply.github.com>
2020-03-05 22:10:22 +0100

    Cambios en Inicio

--- src/Inicio.svelte
+++ src/Inicio.svelte
@@ -1,3 +1,17 @@
+<style>
+  h3 {
+    font-weight: normal;
+    font-size: 20px;
+    margin-left: 10px;
+  }
+</style>
 
-  <h1>Página de Inicio</h1>
-  <p>Bienvenido al Restaurante</p>
+<h1>Bienvenido al Restaurante Frontend</h1>
+<h3>
+  Accede a una sección del menú y añade nuevos platos o cocineros, modifícalos o
+  bórralos
+</h3>
+<h3>
+  Esta web funciona sobre el
+  <a href="https://restaurantebackend.herokuapp.com/">Restaurante Backend.</a>
+</h3>
